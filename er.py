5eedf8787cc6a8a8b692fe11df56fbbe8f6a05b6
f4cax <g53705046@gmail.com>
2025-01-22 23:45:13 +0300

    oo da ento KT1

--- er.py
+++ er.py
@@ -1,12 +1,17 @@
-connection = True
+import random
 
-while connection:
-    email = input("Пожалуйста, введите ваш email: ")
-    
-    if '.' in email and '@' in email:
-        print("Спасибо! Ваш email успешно записан.")
-        connection = False
-    else:
-        print("Введенный адрес недействителен. Адрес должен содержать символы '.' и '@'")
-        print("Пожалуйста, попробуйте еще раз.")
+connection = random.random() < 0.6
+
+if connection:
+    while connection:
+        email = input("Пожалуйста, введите ваш email: ")
+        
+        if '.' in email and '@' in email:
+            print("Спасибо! Ваш email успешно записан.")
+            connection = False
+        else:
+            print("Введенный адрес недействителен. Адрес должен содержать символы '.' и '@'")
+            print("Пожалуйста, попробуйте еще раз.")
+else:
+    print("Нет соединения с сервером. Попробуйте позже.")
 
